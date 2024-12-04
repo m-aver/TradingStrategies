4c8c2575811d49aa338423754c558c00f6ac62f5
m-aver <retikuls@gmail.com>
2024-12-05 01:58:39 +0300

    add benchmarks

---
+++ Benchmarks/BarsDictionaryBenchmark_Read.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Frozen;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradingStrategies.Utilities;
+using WealthLab;
+
+// LiteDictionary в 4-5 раз быстрее на чтение
+// на создание сопоставимо с обычным словарем как по памяти, так и по времени
+
+namespace TradingStrategies.Benchmarks;
+
+public class BarsDictionaryBenchmark_Read
+{
+    [Params(10, 100)]
+    public int SeriesCount;
+    [Params(1, 100)]
+    public int FetchFactor;
+
+    [ParamsAllValues]
+    public BarsType BarsType;
+    [ParamsAllValues]
+    public BarsMapType MapType;
+
+    private Bars[] bars = null!;
+    private Node[] nodes = null!;
+    private IReadOnlyDictionary<Bars, Node> map = null!;
+
+    [GlobalSetup]
+    public void PrepareBars()
+    {
+        bars = BarsDictionaryBenchmarkData.GetBars(SeriesCount, BarsType, MapType);
+        nodes = new Node[bars.Length];
+        map = BarsDictionaryBenchmarkData.CreateMap(bars, nodes, MapType);
+    }
+
+    [Benchmark]
+    public void Read()
+    {
+        for (int i = 0; i < FetchFactor; i++)
+        {
+            foreach (var bar in bars)
+            {
+                var node = map[bar];
+            }
+        }
+    }
+}
---
+++ Benchmarks/Program.cs
@@ -0,0 +1,72 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using TradingStrategies.Benchmarks;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using BenchmarkDotNet.Running;
+
+//ConcurrencyVisualizerProfiler проблемы - DirectoryNotFoundException
+//переименовал директорию проекта с TradingStrategies.Benchmarks на Benchmarks, кажется помогло
+
+internal class Program
+{
+    private static void RunSynchronizedBarIteratorBenchmark()
+    {
+        var config = ManualConfig
+            .Create(DefaultConfig.Instance)
+            .AddJob(Job.Default
+                .With(Platform.AnyCpu)
+                .WithGcServer(true)
+                .WithGcConcurrent(true)
+                .WithGcForce(true)
+            )
+            //.AddDiagnoser(new ConcurrencyVisualizerProfiler())
+            .AddDiagnoser(new EtwProfiler(new EtwProfilerConfig(kernelKeywords: KernelTraceEventParser.Keywords.All)))
+            .AddDiagnoser(new MemoryDiagnoser());
+
+        BenchmarkRunner.Run<SynchronizedBarIteratorBenchmark>(config);
+    }
+
+    private static void RunBarsDictionaryCreateBenchmark()
+    {
+        var config = ManualConfig
+            .Create(DefaultConfig.Instance)
+            .AddJob(Job.Default
+                .With(Platform.AnyCpu)
+                .WithGcForce(true)
+                )
+            .AddDiagnoser(new MemoryDiagnoser());
+
+        BenchmarkRunner.Run<BarsDictionaryBenchmark_Create>(config);
+    }
+
+    private static void RunBarsDictionaryReadBenchmark()
+    {
+        var config = ManualConfig
+            .Create(DefaultConfig.Instance)
+            .AddJob(Job.Default
+                .With(Platform.AnyCpu)
+                .WithGcForce(true)
+                //.WithInvocationCount()
+                //.WithUnrollFactor()
+                );
+
+        BenchmarkRunner.Run<BarsDictionaryBenchmark_Read>(config);
+    }
+
+    private static void Main(string[] args)
+    {
+        try
+        {
+            //RunSynchronizedBarIteratorBenchmark();
+            //RunBarsDictionaryCreateBenchmark();
+            RunBarsDictionaryReadBenchmark();
+        }
+        finally
+        {
+            Console.ReadKey();
+        }
+    }
+}
