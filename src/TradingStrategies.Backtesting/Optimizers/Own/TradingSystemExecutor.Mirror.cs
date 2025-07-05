530d0e0a94afaaeaa398e7d88e7f073925227c43
m-aver <retikuls@gmail.com>
2025-07-05 18:44:48 +0300

    move commented

--- src/TradingStrategies.Backtesting/Optimizers/Own/TradingSystemExecutor.Mirror.cs
+++ src/TradingStrategies.Backtesting/Optimizers/Own/TradingSystemExecutor.Mirror.cs
@@ -64,4 +64,81 @@ public partial class TradingSystemExecutorOwn
     internal List<Position> CurrentPositions { get => tse.CurrentPositions; }
     internal List<Alert> CurrentAlerts { get => tse.CurrentAlerts; }
     internal List<Position> ActivePositions { get => tse.ActivePositions; }
+
+    //own fields
+    /*
+    public int StrategyWindowID;
+    private Bars _barsBeingProcessed;
+    private double _overrideShareSize;
+    private bool _riskStopLevelNotSet;
+    private double _cashRate;
+    private double _marginRate;
+    private double _cashAdjustmentFactor;
+    private double _marginAdjustmentFactor;
+    private double _autoProfitLevel;
+    private PosSizer posSizer;
+    private Position position_0;
+    private bool _rawProfitMode = true;
+    private WealthScript _wealthScriptExecuting;
+    private List<Alert> _masterAlerts = new();
+    private IList<Bars> _barsSet;
+
+    private static PositionSize positionSize = new PositionSize(PosSizeMode.RawProfitShare, 1.0);
+    public static List<PosSizer> PosSizers = null;
+
+    public Strategy Strategy { get; set; }
+    public string DividendItemName { get; set; }
+    public FundamentalsLoader FundamentalsLoader { get; set; }
+    public PositionSize PosSize { get; set; } = new PositionSize();
+    public Commission Commission { get; set; }
+    public bool ApplyCommission { get; set; }
+    public bool ApplyInterest { get; set; }
+
+    public double CashRate
+    {
+        get => _cashRate;
+        set => _cashAdjustmentFactor = CalcAdjustmentFactor(_cashRate = value);
+    }
+    public double MarginRate
+    {
+        get => _marginRate; 
+        set => _marginAdjustmentFactor = CalcAdjustmentFactor(_marginRate = value);
+    }
+    public double OverrideShareSize
+    {
+        get => _overrideShareSize;
+        set
+        {
+            _overrideShareSize = value; 
+            if (PosSize.Mode == PosSizeMode.ScriptOverride) 
+                PosSize.OverrideShareSize = value;
+        }
+    }
+
+    private static double CalcAdjustmentFactor(double rate) => Math.Exp(Math.Log(1.0 + rate / 100.0) / 365.25);
+
+    public bool ApplyDividends { get; set; }
+    public bool BuildEquityCurves { get; set; } = true;
+    public DataSource DataSet { get; set; }
+    public bool ReduceQtyBasedOnVolume { get; set; }
+    public double RedcuceQtyPct { get; set; } = 10.0;
+    public SystemPerformance Performance { get; set; }
+    public bool WorstTradeSimulation { get; set; }
+    public string BenchmarkSymbol { get; set; }
+    public bool EnableSlippage { get; set; }
+    public bool LimitOrderSlippage { get; set; }
+    public double SlippageUnits { get; set; } = 1.0;
+    public int SlippageTicks { get; set; } = 1;
+    public bool LimitDaySimulation { get; set; }
+    public bool RoundLots { get; set; }
+    public bool RoundLots50 { get; set; }
+    public int PricingDecimalPlaces { get; set; }
+    public bool NoDecimalRoundingForLimitStopPrice { get; set; }
+    internal double RiskStopLevel { get; set; }
+
+    public List<Position> MasterPositions { get; } = new();
+    internal List<Position> CurrentPositions { get; } = new();
+    internal List<Alert> CurrentAlerts { get; } = new();
+    internal List<Position> ActivePositions { get; } = new();
+    //*/
 }
